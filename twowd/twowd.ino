5c40914fabc2cfb6a3cc61c71e2963ead42c8faf
Lei <Josefurei2019@gmail.com>
2026-02-21 23:45:41 +0800

    update css

--- twowd/twowd.ino
+++ twowd/twowd.ino
@@ -15,7 +15,7 @@ const int motorB_IN1 = 19; // Direction 1
 const int motorB_IN2 = 21; // Direction 2
 
 WebServer server(80);
-int currentSpeed = 200; 
+int currentSpeed = 200;
 
 // HTML Interface
 // The R"=====( ... )=====" allows to write raw HTML
@@ -25,7 +25,7 @@ const char HTML_CONTENT[] PROGMEM = R"=====(
 <head>
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <style>
-    body { font-family: sans-serif; text-align: center; background: #222; color: white; }
+    body { user-select:none; font-family: sans-serif; text-align: center; background: #222; color: white; }
     .btn { width: 80px; height: 80px; margin: 10px; background: #444; color: #00e676; border: 2px solid #00e676; border-radius: 10px; font-size: 24px; }
     .btn:active { background: #00e676; color: black; }
     .slider { width: 80%; margin-top: 40px; }
@@ -33,13 +33,13 @@ const char HTML_CONTENT[] PROGMEM = R"=====(
 </head>
 <body>
   <h2>Robot Control</h2>
-  <div><button class="btn" ontouchstart="send('forward')" ontouchend="send('stop')">▲</button></div>
+  <div><button class="btn" ontouchstart="send('forward')" ontouchend="send('stop')">F</button></div>
   <div>
-    <button class="btn" ontouchstart="send('left')" ontouchend="send('stop')">◀</button>
-    <button class="btn" ontouchstart="send('right')" ontouchend="send('stop')">▶</button>
+    <button class="btn" ontouchstart="send('left')" ontouchend="send('stop')">L</button>
+    <button class="btn" ontouchstart="send('right')" ontouchend="send('stop')">R</button>
   </div>
-  <div><button class="btn" ontouchstart="send('backward')" ontouchend="send('stop')">▼</button></div>
-  
+  <div><button class="btn" ontouchstart="send('backward')" ontouchend="send('stop')">B</button></div>
+ 
   <p>Speed: <input type="range" min="150" max="255" class="slider" onchange="fetch('/speed?val=' + this.value)"></p>
 
   <script>
@@ -59,7 +59,7 @@ void drive(int speedA, int speedB, int a1, int a2, int b1, int b2) {
 
 void setup() {
   Serial.begin(115200);
-  
+ 
   // Set all motor pins as outputs
   pinMode(motorA_PWM, OUTPUT); pinMode(motorA_IN1, OUTPUT); pinMode(motorA_IN2, OUTPUT);
   pinMode(motorB_PWM, OUTPUT); pinMode(motorB_IN1, OUTPUT); pinMode(motorB_IN2, OUTPUT);
@@ -75,7 +75,7 @@ void setup() {
   // the HTML_CONTENT will be served / shown to the user,
   // it contains the controls as written in the html
   server.on("/", []() { server.send(200, "text/html", HTML_CONTENT); });
-  
+ 
   // These will run based on what button the user taps
   server.on("/forward",  []() { drive(currentSpeed, currentSpeed, 1, 0, 1, 0); server.send(200); });
   server.on("/backward", []() { drive(currentSpeed, currentSpeed, 0, 1, 0, 1); server.send(200); });
